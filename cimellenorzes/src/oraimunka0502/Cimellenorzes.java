dcc2c8b138fb20e3e129d80e8c7ce469a4b29b25
Szazidayum <szabrinamolnar@gmail.com>
2022-05-02 13:51:52 +0200

    masodik módosítások

--- cimellenorzes/src/oraimunka0502/Cimellenorzes.java
+++ cimellenorzes/src/oraimunka0502/Cimellenorzes.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Cimellenorzes {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in,"ISO-8859-1");
         boolean helyesE;
         System.out.print("Kérek egy címet: ");
         String cim = sc.nextLine();
@@ -30,13 +30,19 @@ public class Cimellenorzes {
         System.out.println("A cím 3 szóból áll-e: "+haromHossz);
         
 //      Második szó: utca,tér,út?
-        boolean masodikSzoJo;
-        System.out.println(reszek[1]);
-        if(reszek[1]=="utca" || reszek[1]=="tér" || reszek[1]=="út"){
+        boolean masodikSzoJo=false;
+//        System.out.println(reszek[1]);
+        if("utca".equals(reszek[1]) || "tér".equals(reszek[1]) || "út".equals(reszek[1])){
             masodikSzoJo=true;
-        }else{
-            masodikSzoJo=false;
         }
         System.out.println("A második szó jó: "+masodikSzoJo);
+        
+//      A harmadik szó pontra végződik-e?
+        boolean pontE;
+        String harmadikSzo=reszek[2];
+        char pont=harmadikSzo.charAt(harmadikSzo.length()-1);
+        System.out.println(pont);
+        
     }
+        
 }
