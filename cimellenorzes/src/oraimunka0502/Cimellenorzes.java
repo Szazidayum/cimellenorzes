7afe6724128de455e0a6eff3a16bb73cce555b52
Szazidayum <szabrinamolnar@gmail.com>
2022-05-05 18:24:38 +0200

    kész program Szabrina részéről

--- cimellenorzes/src/oraimunka0502/Cimellenorzes.java
+++ cimellenorzes/src/oraimunka0502/Cimellenorzes.java
@@ -2,14 +2,40 @@ package oraimunka0502;
 
 import java.util.Scanner;
 
+
+
 public class Cimellenorzes {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in,"ISO-8859-1");
-        boolean helyesE;
-        System.out.print("Kérek egy címet: ");
-        String cim = sc.nextLine();
-        String[] reszek = cim.split(" ");       
+        boolean helyesE=false;
+               
+            while(helyesE==false){
+                System.out.print("Kérek egy címet: ");
+                String cim = sc.nextLine();
+                String[] reszek = cim.split(" ");
+              if(haromSzo(reszek)==true && ElsoBetu(reszek)==true && MasodikSzo(reszek)==true && PontE(reszek)==true && SzamE(reszek)==true){
+                helyesE=true;
+                }else{
+                  System.out.println("Nem helyes címet adott meg!");
+              }
+            }
+        System.out.println("A címet helyesen adta meg!");
+
+    } 
+        static boolean haromSzo(String[] reszek){
+//      3 szóból áll-e a cím? 
+        boolean haromHossz;
+        if(reszek.length==3){
+            haromHossz=true;
+            
+        }else{
+            haromHossz=false;
+        }
+//        System.out.println("A cím 3 szóból áll-e: "+haromHossz);
+        return haromHossz;
+    } 
         
+    static boolean ElsoBetu(String[] reszek){
 //      Első betű nagybetű-e?
         boolean nagybetu;
         String elsoSzo = reszek[0];
@@ -18,31 +44,58 @@ public class Cimellenorzes {
         }else{
             nagybetu=false;
         }
-        System.out.println("Az első betű nagybetű-e: "+nagybetu);
+//        System.out.println("Az első betű nagybetű-e: "+nagybetu);
+        return nagybetu;
+    }
         
-//      3 szóból áll-e a cím?
-        boolean haromHossz;
-        if(reszek.length==3){
-            haromHossz=true;
-        }else{
-            haromHossz=false;
-        }
-        System.out.println("A cím 3 szóból áll-e: "+haromHossz);
-        
+    static boolean MasodikSzo(String[] reszek){
 //      Második szó: utca,tér,út?
         boolean masodikSzoJo=false;
 //        System.out.println(reszek[1]);
         if("utca".equals(reszek[1]) || "tér".equals(reszek[1]) || "út".equals(reszek[1])){
             masodikSzoJo=true;
         }
-        System.out.println("A második szó jó: "+masodikSzoJo);
-        
+//        System.out.println("A második szó jó: "+masodikSzoJo);
+        return masodikSzoJo;
+    }
+   
+       static boolean PontE(String[] reszek){
 //      A harmadik szó pontra végződik-e?
         boolean pontE;
         String harmadikSzo=reszek[2];
-        char pont=harmadikSzo.charAt(harmadikSzo.length()-1);
-        System.out.println(pont);
+        char ponT=harmadikSzo.charAt(harmadikSzo.length()-1);
+//        System.out.println(pont);
+        String pont=".";  
+        char p=pont.charAt(0);
+        if(ponT==p){
+            pontE=true;
+        }else{
+            pontE=false;
+        }
+//           System.out.println("Az utolsó karakter pont-e: "+pontE);
+           return pontE;
+        }
+       
+        static boolean SzamE(String[] reszek){
+//      A harmadik szó pont előtti része egy egész szám 1 és 500 között?
+//        System.out.println(harmadikSzo);
+        boolean szamE;
+        String harmadikSzo=reszek[2];
+        String harmadikS = "";
+           for (int i = 0; i < harmadikSzo.length()-1; i++) {
+               harmadikS += harmadikSzo.charAt(i);
+           }
+//           System.out.println(harmadikS);
+        int szam=Integer.parseInt(harmadikS);
+//           System.out.println(szam);
+        if(0<szam && szam<501){
+            szamE=true;
+        }else{
+            szamE=false;
+        }
+//           System.out.println("A szám megfelelő: "+szamE);
+           return szamE;
+       }
         
-    }
-        
+
 }
